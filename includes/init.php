72c8165aba245b3eda411b8bbbc5c1ec05c3d93b
BioWolf <contact.biowolf@gmx.fr>
2020-04-20 19:22:52 +0200

    separated router mapping from index file, and tested Plates for rendering files

--- includes/init.php
+++ includes/init.php
@@ -2,5 +2,6 @@
 define('ROOT', dirname(__DIR__) . '/');
 require_once(ROOT . 'vendor/autoload.php');
 require_once('config.php');
+require_once('routes.php');
 
 App\Session::init();
--- public/index.php
+++ public/index.php
@@ -3,21 +3,11 @@ declare(strict_types=1);
 
 require_once('../includes/init.php');
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-
 $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
     $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
 );
 
-$router = new League\Route\Router;
-
-// map a route
-$router->map('GET', '/', function (ServerRequestInterface $request) : ResponseInterface {
-    $response = new Laminas\Diactoros\Response;
-    $response->getBody()->write('<h1>Hello, World!</h1>');
-    return $response;
-});
+$router = new_router();
 
 $response = $router->dispatch($request);
 
