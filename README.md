8da8668cd94b9d92e16ecd6cf83bd5fb4b0b5fb9
BioWolf <contact.biowolf@gmx.fr>
2020-04-21 15:50:10 +0200

    Created final front-end template

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 A PHP-based SSO auth server built using 
 - Route from ThePHPLeague (league/route in composer) for routes and middleware
 - Plates from ThePHPLeague (league/plates in composer) for view rendering
+
+### Credits:
+- Background image: Image by [Stampf](https://pixabay.com/users/Stampf-1703749/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3337447) from [Pixabay](https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3337447), modified with Paint.net to be darkened and blurred
+- Brand logo: profile picture of [Gray Wolf Games](https://twitter.com/graywolfgames) passed through a sepia filter
--- templates/template.php
+++ templates/template.php
@@ -1,8 +1,23 @@
 <html>
 <head>
     <title><?=$this->e($title)?></title>
+    <link rel="stylesheet" href="/assets/css/spectre.min.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 <body>
+
+<header class="navbar">
+  <section class="navbar-section">
+    <a href="/" class="btn btn-link">SSO Auth server</a>
+  </section>
+  <section class="navbar-center">
+    <img src="/assets/img/logo.png" alt="LOGO">
+  </section>
+  <section class="navbar-section">
+    <a href="https://github.com/BioTheWolff" class="btn btn-link">Made by Fabien "BioTheWolff" Z.</a>
+    <a href="https://github.com/BioTheWolff/SSO-auth-server" class="btn btn-link">GitHub project</a>
+  </section>
+</header>
 
 <?=$this->section('content')?>
 
